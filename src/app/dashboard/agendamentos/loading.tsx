7ac35bc8da2e1796de79afae3a99f7a42e00f7f0
Synkroo Developer <developer@synkroo.com>
2026-06-16 15:49:40 -0300

    feat: polish agendamentos states

--- src/app/dashboard/agendamentos/loading.tsx
+++ src/app/dashboard/agendamentos/loading.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 export default function AgendamentosLoading() {
   return (
     <div className="p-4 lg:p-8 space-y-6">
+      {/* Shell skeleton: header + summary bar + grid */}
       <div className="flex items-center justify-between">
         <div>
           <Skeleton className="h-7 w-36 mb-1" />
@@ -11,20 +12,15 @@ export default function AgendamentosLoading() {
         <Skeleton className="h-10 w-40 rounded-lg" />
       </div>
 
-      <Skeleton className="h-10 w-full rounded-lg" />
-
-      <div className="rounded-xl border border-border">
-        {[1, 2, 3, 4].map((i) => (
-          <div key={i} className="flex items-center gap-4 p-4 border-b border-border last:border-0">
-            <Skeleton className="h-10 w-10 rounded-full" />
-            <div className="flex-1">
-              <Skeleton className="h-4 w-40 mb-1" />
-              <Skeleton className="h-3 w-24" />
-            </div>
-            <Skeleton className="h-6 w-16 rounded-full" />
-          </div>
-        ))}
+      {/* Summary bar skeleton */}
+      <div className="grid gap-3 md:grid-cols-3">
+        <Skeleton className="h-24 w-full rounded-xl" />
+        <Skeleton className="h-24 w-full rounded-xl" />
+        <Skeleton className="h-24 w-full rounded-xl" />
       </div>
+
+      {/* Calendar grid skeleton */}
+      <Skeleton className="h-[500px] w-full rounded-xl" />
     </div>
   )
 }
