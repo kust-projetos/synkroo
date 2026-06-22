687200ee28800484cbda5e01800a172e597e23f8
Synkroo Developer <developer@synkroo.com>
2026-06-22 10:39:02 -0300

    fix(core): register missing core actions

--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -1,7 +1,19 @@
 import { assignUserAccess } from './actions/assign-user-access';
 import { createRole } from './actions/create-role';
 import { setModuleContract } from './actions/set-module-contract';
+import { listClinicUsers } from './actions/list-clinic-users';
+import { listClinicRoles } from './actions/list-clinic-roles';
+import { removeUserAccess } from './actions/remove-user-access';
+import { deactivateUser } from './actions/deactivate-user';
 
-export const coreActions = [assignUserAccess, createRole, setModuleContract];
+export const coreActions = [
+  assignUserAccess,
+  createRole,
+  setModuleContract,
+  listClinicUsers,
+  listClinicRoles,
+  removeUserAccess,
+  deactivateUser,
+];
 export { coreManifest } from './manifest';
 export { coreAccessPermissions } from './permissions';
