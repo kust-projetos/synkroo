506d40aedf445bfae51d3f12d47cedcbad18104c
Synkroo Developer <developer@synkroo.com>
2026-09-14 00:53:13 -0300

    refactor: remove shim deprecated sem consumidores (followup-dispatch)

--- src/services/followup/dispatch-campaign-recipient.ts
+++
@@ -1,6 +0,0 @@
-/** @deprecated Use the Follow-up module's public outbox consumer. */
-export {
-  dispatchCampaignRecipientJob,
-  dispatchNextCampaignRecipient,
-  markCampaignRecipientDeadLetter,
-} from '@/modules/followup/services/dispatch-campaign-recipient';
